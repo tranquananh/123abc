0d5b43d9fefe454feb9d35d73ba805e0aa9a6962
tranquananh <thamtth2302015@fpt.edu.vn>
2023-04-12 15:30:09 +0700

    js

---
+++ .vscode/JS scipt.js
@@ -0,0 +1,120 @@
+ alert(Hello, World);
+//JavaScript Symtax aka cu phap cua javasript
+
+//variable
+//var
+//const
+//let
+let fomatted=true;
+if(fomatted){
+    consonle.log(JavaScript so de qua ma!!);
+}
+//Statements
+let massage="Wello to my website;
+console.log(massage)
+
+Let (window.localStorage){
+    console.log('JavaScript support localstorage');
+}
+//indentifiers
+//a-z,A-Z,_,$,0-9
+let a1;
+let a_b;
+let abc$;
+//comments
+//singer line comments
+/*block
+commment
+here */
+
+//expression bieu thuc giong C program
+
+//let ex=1+1;
+
+//data type
+let couter=100;//couter is a number
+console.log(tyeof(counter));
+
+coiuter=false;//boolean
+console.log(typeof(counter));
+
+let data ;
+console.log(data);
+consolee.log(typeof(data));//undefined->k xd kieu du lieu
+
+//object data type
+let obj=null;
+ocnsole.log(typeof obj));//object
+
+//...
+let num=100;
+let price=20.2;
+let discount=0.05;
+//check min and max number in Javasript
+console.log(number.max_value);
+console.log(number.min_value);
+
+//NaN:not a number
+console.log('t/z');//NaN
+console.log(NaN/2);//NaN
+
+//ket hop string "+"
+let dtr='Jave';
+str=str+'script';
+console.log(str);
+
+//string 
+let s='Javascript';
+s[4]='s';
+console.log(s);
+
+//boolean 
+consloe.log(boolean('connect'));//true
+console.log(boolean(''));//false
+
+//object type
+let studentObject={};
+console.log(typeof(studentObjct0));
+
+//khai bao doi tuong :emloyee
+//khai bao thuoc tinh cua object:firstname,lastname
+let employee={
+    firstname:'Tran'
+    lastname:'Quan Anh'
+};
+//try cap thuoc tinh cua doi tuong :object:firstname,latsname
+console.log(employee.firstname);
+console.log(employee.lastnam);
+//contact detail
+let contact ={
+firstname:'Tran',
+lastnam:'Quan Anh',
+eamil:'tranquananh@gami.com',
+phone:'0999999999',
+address:{
+    buiding:'1000',
+    street:'HBT',
+    city:'Bac Ninh',
+    country:'Viet Nam',
+}
+}
+//de truy cap vao thuoc tinh(propery)cua foi tuong(object)
+//su dung.or[]:contact.firstname or contact['firstname']
+console.og(contact['age']);adding property
+//deleete property
+delete contact.email;
+contact.firstnam="Dang";
+//console.log(contact);
+
+//checkingif a property exists?
+//propertyName in objectName 
+let studenet={
+    firstname:'Tran Dang';
+    lastname:'Cat Tuong';
+    studentId:100;
+};
+console.log('age'in student);//false
+console.log('studentId' in student);//true
+
+
+
---
+++ .vscode/pro.js
@@ -0,0 +1,87 @@
+//function aka Ham
+//Khai bao
+function functionName(paramesret0{
+//functtion body
+
+}
+function getStudentById(id){
+//function body
+//....
+}
+//Example
+//definition of function
+function say(massage){
+ consolẹlog(message);
+}
+//calling function
+let result=say('Hello World);
+console.log('Result:',result);
+
+//with return. 
+function add(a,b){
+   retunr a+b;
+}
+let sum=add (5,10);
+console.log(Sum is:',sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
